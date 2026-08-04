d77db5eea9fc8c0fa34dc130925d7481335ef24b
krishtanish <c.sridaran@gmail.com>
2026-08-05 00:39:33 +0530

    Update mobile API to production URL

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -2,11 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL || Platform.select({
-  android: 'http://10.238.2.243:5119/api',
-  ios: 'http://localhost:5119/api',
-  default: 'http://localhost:5119/api',
-});
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'https://mongift.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
